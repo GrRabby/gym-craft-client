ec5b039001f62bc62e4b65a89f2f99647c879172
GrRabby <grrabby9@gmail.com>
2026-06-22 22:04:41 +0600

    Added better auth lib

---
+++ src/lib/auth.js
@@ -0,0 +1,28 @@
+import { betterAuth } from "better-auth";
+import { jwt } from "better-auth/plugins"
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+const client = new MongoClient(process.env.MONNGODB_URL);
+const db = client.db('gym-craft');
+export const auth = betterAuth({
+    plugins: [
+        jwt()
+    ],
+    session : {
+        cookieCache : {
+            enabled : true,
+            strategy : "jwt",
+            maxAge : 7*24*60*60
+        }
+    },
+    emailAndPassword: {
+        enabled: true,
+    },
+    socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
+        }, 
+    },
+    database: mongodbAdapter(db, {client}),
+});
