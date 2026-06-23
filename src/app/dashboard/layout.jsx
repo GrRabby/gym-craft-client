210e5fa447a18cfe2ea4f91df46516ca9ab3f3d0
GrRabby <grrabby9@gmail.com>
2026-06-23 19:58:00 +0600

    added Admin Routes

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -2,13 +2,13 @@ import DashboardNavbar from "@/app/components/Dashboardnavbar";
 import DashboardSidebar from "@/app/components/Dashboardsidebar";
 
 
-export default function MemberDashboardLayout({ children }) {
+export default function DashboardLayout ({ children }) {
     return (
         <div className="flex min-h-screen bg-[#070707]">
             <DashboardSidebar />
             <main className="flex-1 lg:ml-[280px]">
                 <DashboardNavbar></DashboardNavbar>
-                {children}
+                <main className="p-6 lg:p-10">{children}</main>
             </main>
         </div>
     );
